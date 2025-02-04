5bf0b258057914437a1ca05950b0bac72c8f4001
Ubuntu <ubuntu@ip-172-31-88-153.ec2.internal>
2025-02-04 16:48:06 +0000

    add

---
+++ intermediate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+<< dis
+this is only a block
+dis
+
+#if-else statement
+
+read -p "enter name here:" name
+if [[ $name == "akshu" ]]
+then
+   echo "this name is given"
+else 
+   echo "this name is not given"
+fi
