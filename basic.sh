0217ef85760d425351787d33465f078a6b347da0
Ubuntu <ubuntu@ip-172-31-88-153.ec2.internal>
2025-02-04 06:48:42 +0000

    add

---
+++ basic.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#basic
+echo "hello how are you?"
+
+
+#using read permission
+echo "what is your name?"
+read name
+echo "hello,$name"
+
+
+#using variable
+name="aakansha"
+city="kolad"
+echo "hello,i am $name,and i am from $city"
+
